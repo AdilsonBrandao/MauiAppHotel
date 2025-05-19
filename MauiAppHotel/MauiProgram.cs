2f922535df5282d9305523fb58e2674c621fc5f7
AdilsonBrandao <adilsonbaptistabrandao2012@hotmail.com>
2025-05-19 18:43:13 -0300

    Add recursos ao app

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,7 +13,12 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                    fonts.AddFont("Anton-Regular.ttf", "AntonRegular");
+                    fonts.AddFont("Codystar-Regular.ttf", "CodystarRegular");
+                    fonts.AddFont("ComicRelief-Regular.ttf", "ComicReliefRegular");
+                    fonts.AddFont("DancingScript-Regular.ttf", "DancingScriptRegular");
+                    fonts.AddFont("AlfaSlabOne-Regular.ttf", "AlfaSlabOneRegular");
+                }); 
 
 #if DEBUG
     		builder.Logging.AddDebug();
